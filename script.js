44cbed5419b586b3ee24da1ed942739e6a137b23
Mona-chandrika <24WH1A0570@bvrithyderabad.edu.in>
2026-03-12 15:56:02 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+const text = "Welcome to Team ZENVIRA";
+
+let i = 0;
+
+function typing(){
+
+if(i < text.length){
+
+document.getElementById("typing").innerHTML += text.charAt(i);
+
+i++;
+
+setTimeout(typing,60);
+
+}
+
+}
+
+typing();
